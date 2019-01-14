3a9109f16ea84f9f8a67da97e4cbe11eae6218f5
LubnaFazalShaikh <46677329+LubnaFazalShaikh@users.noreply.github.com>
2019-01-14 14:19:33 +0100

    Project Commit

---
+++ UnitTestAnimalFeedingCalculator/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using  a;
+
+namespace UnitTestAnimalFeedingCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Food fd = new Food();
+
+        }
+    }
+}
